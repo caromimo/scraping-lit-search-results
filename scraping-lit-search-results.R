d93ed07d5b93be3eb4a582e8b8ec858559c6ee3a
Caroline Mimeault <carolinemimeault@gmail.com>
2021-10-31 16:11:47 -0400

    adds pagination to the scraper

--- scraping-lit-search-results.R
+++ scraping-lit-search-results.R
@@ -2,6 +2,10 @@
 # C. Mimeault and M. Williamson
 # October 2021
 
+# link to the Federal Science Libraries Network
+# https://science-libraries.canada.ca/eng/home/
+# searches are not case sensitive
+
 # clear memory
 remove(list=ls())
 knitr::opts_chunk$set(echo = TRUE)
@@ -10,38 +14,148 @@ knitr::opts_chunk$set(echo = TRUE)
 library(rvest)
 library(tidyverse)
 library(glue)
+library(urltools)
 
-# identify webpage (with search results)
-# (("British Columbia" OR BC ) AND ("Chinook salmon" OR "Oncorhynchus tshawytscha") AND (juvenile) AND (riddell OR beamish) 
-# 12 results (2021-10-30)
-url <- "https://science-libraries.canada.ca/eng/search/?q=%28%28%22British+Columbia%22+OR+BC+%29+AND+%28%22Chinook+salmon%22+OR+%22Oncorhynchus+tshawytscha%22%29+AND+%28juvenile%29+AND+%28beamish+OR+trudel+OR+ridell%29&sm=1&fc=Library%3ADFO-MPO&fs=IsLibraryCatalogue%3A1"
+# Initial search ----------------------------------------------------------
 
-# open a session in the browser
-session(url)
+# search terms: (("British Columbia" OR BC ) AND (salmon OR Chinook OR pink OR chum OR coho OR Sockeye) AND (juvenile OR immature))
+# # returns 850 results (Oct 31, 2021) which is too many (maximum allowed is 500 records) 
+# so breaking it down per species
 
-# read html
-html <- read_html(url)
+# ensuring that we can reproduce the results from initial search"
 
-# identify number of pages (maximum of 50 allowed on website)
-# number_of_pages <- c(1:50)
+intial_search_terms <- '(("British Columbia" OR BC ) AND (salmon OR Chinook OR pink OR chum OR coho OR Sockeye) AND (juvenile OR immature))'
 
-# extract titles to explore (just for interest)
-titles <- html %>% 
-  html_nodes("ol.search-results h3") %>%
-  html_text()
+intial_search_url <- "https://science-libraries.canada.ca/eng/search/" %>% 
+  param_set("fc", "Library%3ADFO-MPO") %>% 
+  param_set("fs", "IsLibraryCatalogue%3A1") %>% 
+  param_set("q", url_encode(intial_search_terms)) %>%
+  param_set("sm", 1)
 
-# extract search result id numbers
-id_numbers <- html %>%
-  html_nodes("a.save-search-result") %>%
-  html_attr("id")
+# Chinook salmon search ----------------------------------------------------------
 
-for(id in id_numbers){
+# search terms: 
+# ("British Columbia" OR BC ) AND ("Chinook salmon" OR "Oncorhynchus tshawytscha") AND (juvenile* OR immature* OR smolt*)
+# returns 277 results (Oct 31, 2021)
+
+chinook_search_terms <- '("British Columbia" OR BC) AND ("Chinook salmon" OR "Oncorhynchus tshawytscha") AND (juvenile* OR immature* OR smolt*)'
+
+chinook_search_url <- "https://science-libraries.canada.ca/eng/search/" %>% 
+  param_set("fc", "Library%3ADFO-MPO") %>% 
+  param_set("fs", "IsLibraryCatalogue%3A1") %>% 
+  param_set("q", url_encode(chinook_search_terms)) %>%
+  param_set("sm", 1)
+
+# open and maintain a session across multiple requests (being the various pages and calls to save results)
+results_page <- session(chinook_search_url)
+
+# move to the next page
+while(results_page) {
+  tryCatch({results_page <- session_follow_link(css = "a[rel=next]")},
+           error = function(e) {results_page = FALSE})
+  print("got a page")
+  
+}
+  print("no more pages")
+  
+# TESTING looping ----------------------------------------------------------
+  
+test_search_terms <- '("British Columbia" OR BC) AND ("Chinook salmon" OR "Oncorhynchus tshawytscha") AND (juvenile* OR immature* OR smolt*) AND (neville OR trudel OR riddell)'
+test_search_url <- "https://science-libraries.canada.ca/eng/search/" %>% 
+  param_set("fc", "Library%3ADFO-MPO") %>% 
+  param_set("fs", "IsLibraryCatalogue%3A1") %>% 
+  param_set("q", url_encode(test_search_terms)) %>%
+  param_set("sm", 1)
+
+# open and maintain a session across multiple requests (being the various pages and calls to save results)
+results_page <- session(test_search_url)
+
+get_ids <- function(page){
+  page %>%
+    html_nodes("a.save-search-result") %>%
+    html_attr("id")
+}
+
+save_item_by_id <- function(id){
   print(glue("https://science-libraries.canada.ca/eng/search/save-items/?id={id}"))
 }
 
 # move to the next page
-session(url) %>%
-  session_follow_link(css = "a[rel=next]")
+has.next <- TRUE
+
+while(has.next) {
+  ids <- results_page %>% get_ids()
+  print("here are the fetched ids for this page:")
+  print(ids)
+  print("now saving results to session")
+  for(id in ids){
+    save_item_by_id(id)
+    }
+  tryCatch(
+    {
+      results_page <<- results_page %>% session_follow_link(css = "a[rel=next]")
+      },
+      error = function(e) {
+        has.next <<- FALSE
+        }
+    )
+}
+
+print("there are no more pages (and no next button)")
+print("download a csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Chum salmon search ----------------------------------------------------------
+
+# search terms: 
+# ("British Columbia" OR BC ) AND ("Chum salmon" OR "Oncorhynchus keta") AND (juvenile* OR immature* OR smolt*)
+# returns 129 results (Oct 31, 2021)
+
+# Coho salmon search ----------------------------------------------------------
+
+# search terms: 
+# ("British Columbia" OR BC ) AND ("Coho salmon" OR "Oncorhynchus kisutch") AND (juvenile* OR immature* OR smolt*)
+# returns 406 results (Oct 31, 2021)
+
+# Pink salmon search ----------------------------------------------------------
+
+# search terms: 
+# ("British Columbia" OR BC ) AND ("Pink salmon" OR "Oncorhynchus gorbuscha") AND (juvenile* OR immature* OR smolt*)
+# returns 99 results (Oct 31, 2021)
+
+# Sockeye salmon search ----------------------------------------------------------
+
+# search terms: 
+# ("British Columbia" OR BC ) AND ("Sockeye salmon" OR "Oncorhynchus nerka") AND (juvenile* OR immature* OR smolt*)
+# returns 256 results (Oct 31, 2021)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
